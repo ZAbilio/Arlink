424a2a13dd8a6932dbde1fc2e82628f6080390ef
Laboratorio <laboratorio@escola.local>
2026-09-03 21:43:14 -0300

    Ajusta tela de login

--- login.php
+++ login.php
@@ -59,7 +59,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-   
+
+    <div class="pagina-login">
         <section class="login">
             <h1>ARLINK</h1>
             <p>Gestão de manutenções de ar-condicionado</p>
@@ -99,6 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <button type="submit">Entrar</button>
             </form>
         </section>
-   
+    </div>
+
 </body>
 </html>
